86cac086f3150a8fa166954f986feca68b5aff61
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-08-09 21:43:57 +0200

    fixup

--- app/core/users/tools_users.py
+++ app/core/users/tools_users.py
@@ -15,6 +15,10 @@ async def get_account_type_and_school_id_from_email(
     settings: Settings,
 ) -> tuple[AccountType, UUID]:
     """Return the account type from the email"""
+    if settings.school.student_email_regex.match(
+        email,
+    ):
+        return AccountType.student, SchoolType.base_school.value
     if (
         settings.school.staff_email_regex is not None
         and settings.school.staff_email_regex.match(
@@ -22,10 +26,6 @@ async def get_account_type_and_school_id_from_email(
         )
     ):
         return AccountType.staff, SchoolType.base_school.value
-    if settings.school.student_email_regex.match(
-        email,
-    ):
-        return AccountType.student, SchoolType.base_school.value
     if (
         settings.school.former_student_email_regex is not None
         and settings.school.former_student_email_regex.match(
@@ -33,6 +33,7 @@ async def get_account_type_and_school_id_from_email(
         )
     ):
         return AccountType.former_student, SchoolType.base_school.value
+
     schools = await cruds_schools.get_schools(db)
 
     schools = [school for school in schools if school.id not in SchoolType.list()]
