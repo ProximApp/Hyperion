6749e54256ddc333ffd49b815c8f9096b9ac2572
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-08-16 15:14:11 +0200

    Refactor advert to use associations

--- app/modules/advert/cruds_advert.py
+++ app/modules/advert/cruds_advert.py
@@ -1,69 +1,10 @@
 from collections.abc import Sequence
+from uuid import UUID
 
-from sqlalchemy import delete, or_, select, update
+from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.modules.advert import models_advert, schemas_advert
-
-
-async def get_advertisers(db: AsyncSession) -> Sequence[models_advert.Advertiser]:
-    result = await db.execute(select(models_advert.Advertiser))
-    return result.scalars().all()
-
-
-async def get_advertiser_by_id(
-    db: AsyncSession,
-    advertiser_id: str,
-) -> models_advert.Advertiser | None:
-    result = await db.execute(
-        select(models_advert.Advertiser).where(
-            models_advert.Advertiser.id == advertiser_id,
-        ),
-    )
-    return result.scalars().first()
-
-
-async def get_advertisers_by_groups(
-    db: AsyncSession,
-    user_groups_ids: Sequence[str],
-) -> Sequence[models_advert.Advertiser] | None:
-    result = await db.execute(
-        select(models_advert.Advertiser).where(
-            models_advert.Advertiser.group_manager_id.in_(user_groups_ids),
-        ),
-    )
-    return result.scalars().all()
-
-
-async def create_advertiser(
-    db_advertiser: models_advert.Advertiser,
-    db: AsyncSession,
-) -> models_advert.Advertiser:
-    db.add(db_advertiser)
-    await db.flush()
-    return db_advertiser
-
-
-async def update_advertiser(
-    advertiser_id: str,
-    advertiser_update: schemas_advert.AdvertiserUpdate,
-    db: AsyncSession,
-):
-    await db.execute(
-        update(models_advert.Advertiser)
-        .where(models_advert.Advertiser.id == advertiser_id)
-        .values(**advertiser_update.model_dump(exclude_none=True)),
-    )
-    await db.flush()
-
-
-async def delete_advertiser(advertiser_id: str, db: AsyncSession):
-    await db.execute(
-        delete(models_advert.Advertiser).where(
-            models_advert.Advertiser.id == advertiser_id,
-        ),
-    )
-    await db.flush()
 
 
 async def get_adverts(db: AsyncSession) -> Sequence[models_advert.Advert]:
@@ -73,7 +14,7 @@ async def get_adverts(db: AsyncSession) -> Sequence[models_advert.Advert]:
 
 async def get_advert_by_id(
     db: AsyncSession,
-    advert_id: str,
+    advert_id: UUID,
 ) -> models_advert.Advert | None:
     result = await db.execute(
         select(models_advert.Advert).where(models_advert.Advert.id == advert_id),
@@ -83,18 +24,11 @@ async def get_advert_by_id(
 
 async def get_adverts_by_advertisers(
     db: AsyncSession,
-    advertisers: list[str],
+    advertisers: list[UUID],
 ) -> Sequence[models_advert.Advert]:
     result = await db.execute(
         select(models_advert.Advert).where(
-            or_(
-                *[
-                    models_advert.Advert.advertiser.has(
-                        models_advert.Advertiser.id == advertiser_id,
-                    )
-                    for advertiser_id in advertisers
-                ],
-            ),
+            models_advert.Advert.advertiser_id.in_(advertisers),
         ),
     )
     return result.scalars().all()
@@ -103,27 +37,31 @@ async def get_adverts_by_advertisers(
 async def create_advert(
     db_advert: models_advert.Advert,
     db: AsyncSession,
-) -> models_advert.Advert:
+) -> None:
     db.add(db_advert)
-    await db.flush()
-    return db_advert
 
 
 async def update_advert(
-    advert_id: str,
+    advert_id: UUID,
     advert_update: schemas_advert.AdvertUpdate,
     db: AsyncSession,
 ):
     await db.execute(
         update(models_advert.Advert)
         .where(models_advert.Advert.id == advert_id)
-        .values(**advert_update.model_dump(exclude_none=True)),
+        .values(
+            **advert_update.model_dump(exclude_unset=True),
+        ),
     )
-    await db.flush()
 
 
-async def delete_advert(advert_id: str, db: AsyncSession):
+async def delete_advert(
+    advert_id: UUID,
+    db: AsyncSession,
+):
     await db.execute(
-        delete(models_advert.Advert).where(models_advert.Advert.id == advert_id),
+        delete(models_advert.Advert).where(
+            models_advert.Advert.id == advert_id,
+        ),
     )
     await db.flush()
--- app/modules/advert/factory_advert.py
+++ app/modules/advert/factory_advert.py
@@ -3,33 +3,25 @@ from datetime import UTC, datetime, timedelta
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.groups.groups_type import GroupType
+from app.core.associations.factory_associations import AssociationsFactory
 from app.core.utils.config import Settings
 from app.modules.advert import cruds_advert, models_advert
 from app.types.factory import Factory
 
 
 class AdvertFactory(Factory):
-    depends_on = []
+    depends_on = [AssociationsFactory]
 
     @classmethod
     async def run(cls, db: AsyncSession, settings: Settings) -> None:
-        advertiser = models_advert.Advertiser(
-            id=str(uuid.uuid4()),
-            name="Le BDE",
-            adverts=[],
-            group_manager_id=str(GroupType.BDE.value),
-        )
-
         await cruds_advert.create_advert(
             db=db,
             db_advert=models_advert.Advert(
                 title="Nouveau mandat",
                 content="Nous sommes heureux de vous annoncer notre nouveau mandat !",
-                id=str(uuid.uuid4()),
+                id=uuid.uuid4(),
                 date=datetime.now(UTC),
-                advertiser_id=advertiser.id,
-                advertiser=advertiser,
+                advertiser_id=AssociationsFactory.association_ids[0],
                 post_to_feed=False,
             ),
         )
@@ -39,19 +31,11 @@ class AdvertFactory(Factory):
             db_advert=models_advert.Advert(
                 title="Info M16",
                 content="Ne cassez pas les tables svp !",
-                id=str(uuid.uuid4()),
+                id=uuid.uuid4(),
                 date=datetime.now(UTC) - timedelta(days=1),
-                advertiser_id=advertiser.id,
-                advertiser=advertiser,
+                advertiser_id=AssociationsFactory.association_ids[0],
                 post_to_feed=False,
             ),
-        )
-
-        advertiser2 = models_advert.Advertiser(
-            id=str(uuid.uuid4()),
-            name="Eclair ces bgs",
-            adverts=[],
-            group_manager_id=str(GroupType.eclair.value),
         )
 
         await cruds_advert.create_advert(
@@ -59,10 +43,9 @@ class AdvertFactory(Factory):
             db_advert=models_advert.Advert(
                 title="Les 24h du code",
                 content="Codons comme des fous !",
-                id=str(uuid.uuid4()),
+                id=uuid.uuid4(),
                 date=datetime.now(UTC) - timedelta(days=2),
-                advertiser_id=advertiser2.id,
-                advertiser=advertiser2,
+                advertiser_id=AssociationsFactory.association_ids[1],
                 post_to_feed=False,
             ),
         )
