266848bcf79fdc6122aa48be36f85e03599ab1d2
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-03-01 11:28:23 +0100

    Add CoreAssociation relationship to stores
    # Conflicts:
    #	app/core/mypayment/utils_mypayment.py

--- tests/core/test_mypayment.py
+++ tests/core/test_mypayment.py
@@ -334,6 +334,7 @@ async def init_objects() -> None:
         name="Test Store",
         structure_id=structure.id,
         creation=datetime.now(UTC),
+        association_id=association_membership.id,
     )
     await add_object_to_db(store)
     store2 = models_mypayment.Store(
@@ -342,6 +343,7 @@ async def init_objects() -> None:
         name="Test Store 2",
         structure_id=structure2.id,
         creation=datetime.now(UTC),
+        association_id=association_membership.id,
     )
     await add_object_to_db(store2)
     store3 = models_mypayment.Store(
@@ -350,6 +352,7 @@ async def init_objects() -> None:
         name="Test Store 3",
         structure_id=structure2.id,
         creation=datetime.now(UTC),
+        association_id=association_membership.id,
     )
     await add_object_to_db(store3)
 
@@ -887,6 +890,7 @@ async def test_transfer_structure_manager_as_manager(
         wallet_id=new_wallet.id,
         name="Test Store Structure 2",
         structure_id=new_structure.id,
+        association_id=association_membership.id,
     )
     await add_object_to_db(new_store)
     new_wallet2 = models_mypayment.Wallet(
@@ -901,6 +905,7 @@ async def test_transfer_structure_manager_as_manager(
         wallet_id=new_wallet2.id,
         name="Test Store Structure 2 Where New Manager Already Seller",
         structure_id=new_structure.id,
+        association_id=association_membership.id,
     )
     await add_object_to_db(new_store2_where_new_manager_already_seller)
     seller = models_mypayment.Seller(
@@ -1330,6 +1335,7 @@ async def test_delete_store(client: TestClient):
         wallet_id=new_wallet.id,
         name="Test Store to Delete",
         structure_id=structure.id,
+        association_id=association_membership.id,
     )
     await add_object_to_db(new_store)
     sellet = models_mypayment.Seller(
@@ -1364,6 +1370,7 @@ async def test_update_store(client: TestClient):
         wallet_id=new_wallet.id,
         name="Test Store Update",
         structure_id=structure.id,
+        association_id=association_membership.id,
     )
     await add_object_to_db(new_store)
     response = client.patch(
