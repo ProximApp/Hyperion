b8d45d8b61ec87716b576267aab21c7678a17701
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-03-21 17:25:01 +0100

    Migration rebase

--- migrations/versions/66-stores_coreassociations.py
+++ migrations/versions/66-stores_coreassociations.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "146db8dcb23e"
-down_revision: str | None = "1ec573d854a1"
+down_revision: str | None = "562adbd796ae"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
