4007b812bc4f4bff63a947f4eeaf6c18c449a230
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-03-28 13:05:22 +0100

    Unique constraint

--- tests/core/test_mypayment.py
+++ tests/core/test_mypayment.py
@@ -19,6 +19,7 @@ from app.core.mypayment import cruds_mypayment, models_mypayment
 from app.core.mypayment.coredata_mypayment import (
     MyPaymentBankAccountHolder,
 )
+from app.core.mypayment.cruds_mypayment import delete_store
 from app.core.mypayment.schemas_mypayment import QRCodeContentData
 from app.core.mypayment.types_mypayment import (
     TransactionStatus,
@@ -325,6 +326,12 @@ async def init_objects() -> None:
         association_id=core_association.id,
     )
     await add_object_to_db(store)
+    core_association2 = models_associations.CoreAssociation(
+        id=uuid4(),
+        name="core_association2",
+        group_id=core_association_group.id,
+    )
+    await add_object_to_db(core_association2)
     store2 = models_mypayment.Store(
         id=uuid4(),
         wallet_id=store2_wallet.id,
