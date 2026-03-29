79abad6d5090078a5a3240bfc59c86c6710cec30
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-03-29 15:25:34 +0200

    Mypayment: can_manage_events

---
+++ migrations/versions/67-mypayment.py
@@ -0,0 +1,51 @@
+"""empty message
+
+Create Date: 2026-03-29 15:20:10.468941
+"""
+
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from pytest_alembic import MigrationContext
+
+import sqlalchemy as sa
+from alembic import op
+
+from app.types.sqlalchemy import TZDateTime
+
+# revision identifiers, used by Alembic.
+revision: str = "de94c373f94a"
+down_revision: str | None = "c052cfbe6d75"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "mypayment_seller",
+        sa.Column(
+            "can_manage_events",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.false(),
+        ),
+    )
+
+
+def downgrade() -> None:
+    pass
+
+
+def pre_test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    pass
+
+
+def test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    pass
