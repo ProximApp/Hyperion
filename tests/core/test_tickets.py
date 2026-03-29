b4c7844935434642536d0391073a6c2f23ed7102
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-03-29 22:54:27 +0200

    Add tests

--- tests/core/test_tickets.py
+++ tests/core/test_tickets.py
@@ -30,9 +30,16 @@ core_association: CoreAssociation
 store: Store
 
 
-ticket_event: models_tickets.TicketEvent
-ticket_session: models_tickets.EventSession
-ticket_category: models_tickets.Category
+global_event: models_tickets.TicketEvent
+event_session: models_tickets.EventSession
+event_category: models_tickets.Category
+event_sold_out_category: models_tickets.Category
+event_sold_out_session: models_tickets.EventSession
+
+sold_out_event: models_tickets.TicketEvent
+session_sold_out_event: models_tickets.EventSession
+category_sold_out_event: models_tickets.Category
+
 
 ticket: models_tickets.Ticket
 
@@ -99,17 +106,16 @@ async def init_objects() -> None:
     )
     await add_object_to_db(store)
 
-    global ticket_event, ticket_session, ticket_category
+    global global_event, event_session, event_category
     ticket_event_id = uuid.uuid4()
-    ticket_session = models_tickets.EventSession(
+    event_session = models_tickets.EventSession(
         id=uuid.uuid4(),
         event_id=ticket_event_id,
         name="Test Session",
-        start_time=datetime.now(tz=UTC) - timedelta(days=1),
-        end_time=datetime.now(tz=UTC) + timedelta(days=1),
+        start_datetime=datetime.now(tz=UTC) - timedelta(days=1),
         quota=None,
     )
-    ticket_category = models_tickets.Category(
+    event_category = models_tickets.Category(
         id=uuid.uuid4(),
         event_id=ticket_event_id,
         name="Test Category",
@@ -117,19 +123,96 @@ async def init_objects() -> None:
         price=1000,
         required_membership=None,
     )
-    ticket_event = models_tickets.TicketEvent(
+    global_event = models_tickets.TicketEvent(
         id=uuid.uuid4(),
         store_id=store.id,
-        name="Test Event",
+        name="Test global_event",
         open_datetime=datetime.now(tz=UTC) - timedelta(days=1),
         close_datetime=datetime.now(tz=UTC) + timedelta(days=1),
-        quota_per_user=2,
-        quota_per_checkout=2,
         quota=10,
-        sessions=[ticket_session],
-        categories=[ticket_category],
+        sessions=[event_session],
+        categories=[event_category],
     )
-    await add_object_to_db(ticket_event)
+    await add_object_to_db(global_event)
+
+    global event_sold_out_category, event_sold_out_session
+    event_sold_out_category = models_tickets.Category(
+        id=uuid.uuid4(),
+        event_id=global_event.id,
+        name="Test global_event Sold Out Category",
+        quota=1,
+        price=1000,
+        required_membership=None,
+    )
+    await add_object_to_db(event_sold_out_category)
+    ticket_sold_out_category = models_tickets.Ticket(
+        id=uuid.uuid4(),
+        category_id=event_sold_out_category.id,
+        session_id=event_session.id,
+        event_id=global_event.id,
+        user_id=user.id,
+        price=10,
+        scanned=False,
+    )
+    await add_object_to_db(ticket_sold_out_category)
+    event_sold_out_session = models_tickets.EventSession(
+        id=uuid.uuid4(),
+        event_id=global_event.id,
+        name="Test global_event Sold Out Session",
+        start_datetime=datetime.now(tz=UTC) - timedelta(days=1),
+        quota=1,
+    )
+    await add_object_to_db(event_sold_out_session)
+    ticket_sold_out_session = models_tickets.Ticket(
+        id=uuid.uuid4(),
+        category_id=event_category.id,
+        session_id=event_sold_out_session.id,
+        event_id=global_event.id,
+        user_id=user.id,
+        price=10,
+        scanned=False,
+    )
+    await add_object_to_db(ticket_sold_out_session)
+
+    global sold_out_event, session_sold_out_event, category_sold_out_event
+    ticket_sold_out_event_id = uuid.uuid4()
+    session_sold_out_event = models_tickets.EventSession(
+        id=uuid.uuid4(),
+        event_id=ticket_sold_out_event_id,
+        name="Test Session Sold Out",
+        start_datetime=datetime.now(tz=UTC) - timedelta(days=1),
+        quota=1,
+    )
+    category_sold_out_event = models_tickets.Category(
+        id=uuid.uuid4(),
+        event_id=ticket_sold_out_event_id,
+        name="Test Category Sold Out",
+        quota=1,
+        price=1000,
+        required_membership=None,
+    )
+    sold_out_event = models_tickets.TicketEvent(
+        id=ticket_sold_out_event_id,
+        store_id=store.id,
+        name="Test global_event Sold Out",
+        open_datetime=datetime.now(tz=UTC) - timedelta(days=1),
+        close_datetime=datetime.now(tz=UTC) + timedelta(days=1),
+        quota=1,
+        sessions=[session_sold_out_event],
+        categories=[category_sold_out_event],
+    )
+    await add_object_to_db(sold_out_event)
+    user = await create_user_with_groups(groups=[])
+    ticket_sold_out_event = models_tickets.Ticket(
+        id=uuid.uuid4(),
+        category_id=category_sold_out_event.id,
+        session_id=session_sold_out_event.id,
+        event_id=ticket_sold_out_event_id,
+        user_id=user.id,
+        price=10,
+        scanned=False,
+    )
+    await add_object_to_db(ticket_sold_out_event)
 
 
 def test_get_open_events(client: TestClient):
@@ -138,7 +221,7 @@ def test_get_open_events(client: TestClient):
         headers={"Authorization": f"Bearer {user_token}"},
     )
     assert response.status_code == 200
-    assert len(response.json()) == 1
+    assert len(response.json()) > 1
 
 
 def test_get_event_with_non_existing_id(client: TestClient):
@@ -151,7 +234,128 @@ def test_get_event_with_non_existing_id(client: TestClient):
 
 def test_get_event(client: TestClient):
     response = client.get(
-        f"/tickets/events/{ticket_event.id}",
+        f"/tickets/events/{global_event.id}",
         headers={"Authorization": f"Bearer {user_token}"},
     )
     assert response.status_code == 200
+    event = response.json()
+    assert event["id"] == str(global_event.id)
+    assert len(event["sessions"]) > 0
+    assert len(event["categories"]) > 0
+    assert event["sold_out"] is False
+
+
+def test_get_sold_out_event(client: TestClient):
+    response = client.get(
+        f"/tickets/events/{sold_out_event.id}",
+        headers={"Authorization": f"Bearer {user_token}"},
+    )
+    assert response.status_code == 200
+    event = response.json()
+    assert event["id"] == str(sold_out_event.id)
+    assert len(event["sessions"]) > 0
+    assert len(event["categories"]) > 0
+    assert event["sold_out"] is True
+
+
+def test_create_checkout_with_invalid_category(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{sold_out_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(uuid.uuid4()),
+            "session_id": str(session_sold_out_event.id),
+        },
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Category not found"
+
+
+def test_create_checkout_with_invalid_session(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{sold_out_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(category_sold_out_event.id),
+            "session_id": str(uuid.uuid4()),
+        },
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Session not found"
+
+
+def test_create_checkout_with_category_from_another_event(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{sold_out_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(event_category.id),
+            "session_id": str(session_sold_out_event.id),
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Category does not belong to the event"
+
+
+def test_create_checkout_with_session_from_another_event(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{sold_out_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(category_sold_out_event.id),
+            "session_id": str(event_session.id),
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Session does not belong to the event"
+
+
+# TODO: test required membership
+
+
+def test_create_checkout_with_sold_out_event(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{sold_out_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(category_sold_out_event.id),
+            "session_id": str(session_sold_out_event.id),
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Event is sold out"
+
+
+def test_create_checkout_with_sold_out_category(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{global_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(event_sold_out_category.id),
+            "session_id": str(event_session.id),
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Category is sold out"
+
+
+def test_create_checkout_with_sold_out_session(client: TestClient):
+    response = client.post(
+        f"/tickets/events/{global_event.id}/checkout",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={
+            "category_id": str(event_category.id),
+            "session_id": str(event_sold_out_session.id),
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Session is sold out"
+
+
+def test_get_user_tickets(client: TestClient):
+    response = client.get(
+        "/tickets/user/me/tickets",
+        headers={"Authorization": f"Bearer {user_token}"},
+    )
+    assert response.status_code == 200
+    assert len(response.json()) > 1
