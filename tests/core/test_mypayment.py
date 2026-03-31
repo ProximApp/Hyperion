c21bd178f6b738c180621f5aeb2a967a8a9a3246
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-04-01 01:58:06 +0200

    fix tests

--- tests/core/test_mypayment.py
+++ tests/core/test_mypayment.py
@@ -148,6 +148,7 @@ async def init_objects() -> None:
         name="Association",
         group_id=GroupType.admin,
     )
+    await add_object_to_db(core_association)
 
     global association_membership
     association_membership = models_memberships.CoreAssociationMembership(
@@ -972,6 +973,7 @@ async def test_create_store_for_non_existing_structure(client: TestClient):
         headers={"Authorization": f"Bearer {structure_manager_user_token}"},
         json={
             "name": "test_create_store Test Store",
+            "association_id": str(core_association.id),
         },
     )
     assert response.status_code == 404
@@ -984,6 +986,7 @@ async def test_create_store(client: TestClient):
         headers={"Authorization": f"Bearer {structure_manager_user_token}"},
         json={
             "name": "test_create_store Test Store",
+            "association_id": str(core_association.id),
         },
     )
     assert response.status_code == 201
@@ -1003,6 +1006,7 @@ async def test_create_store_when_user_not_manager_of_structure(client: TestClien
         headers={"Authorization": f"Bearer {ecl_user_access_token}"},
         json={
             "name": "test_create_store Test Store",
+            "association_id": str(core_association.id),
         },
     )
     assert response.status_code == 403
@@ -1015,6 +1019,7 @@ async def test_create_store_with_name_already_exist(client: TestClient):
         headers={"Authorization": f"Bearer {structure_manager_user_token}"},
         json={
             "name": "Test Store",
+            "association_id": str(core_association.id),
         },
     )
     assert response.status_code == 400
