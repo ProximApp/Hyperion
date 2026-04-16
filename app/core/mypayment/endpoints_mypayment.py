5717552061394cdafc67fc8392a7f398776a432b
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-04-16 23:02:27 +0200

    fix

--- app/core/mypayment/endpoints_mypayment.py
+++ app/core/mypayment/endpoints_mypayment.py
@@ -923,17 +923,15 @@ async def update_store(
         )
 
     if store_update.association_id is not None:
-        if store_update.association_id != store.association_id:
-            # If the id does not change, we don't need to check if an other store already exists for this association
-            association = await cruds_associations.get_association_by_id(
-                db=db,
-                association_id=store_update.association_id,
+        association = await cruds_associations.get_association_by_id(
+            db=db,
+            association_id=store_update.association_id,
+        )
+        if not association:
+            raise HTTPException(
+                status_code=404,
+                detail="Association not found",
             )
-            if not association:
-                raise HTTPException(
-                    status_code=404,
-                    detail="Association not found",
-                )
         if not is_user_member_of_an_association(
             user=user,
             association=association,
@@ -942,17 +940,19 @@ async def update_store(
                 status_code=403,
                 detail="You are not allowed to create stores for this association",
             )
-        existing_store_for_association = (
-            await cruds_mypayment.get_store_by_association_id(
-                association_id=store.association_id,
-                db=db,
+        if store_update.association_id != store.association_id:
+            # If the id does not change, we don't need to check if an other store already exists for this association
+            existing_store_for_association = (
+                await cruds_mypayment.get_store_by_association_id(
+                    association_id=store_update.association_id,
+                    db=db,
+                )
             )
-        )
-        if existing_store_for_association is not None:
-            raise HTTPException(
-                status_code=400,
-                detail="Store for this association already exists",
-            )
+            if existing_store_for_association is not None:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Store for this association already exists",
+                )
 
     await cruds_mypayment.update_store(
         store_id=store_id,
