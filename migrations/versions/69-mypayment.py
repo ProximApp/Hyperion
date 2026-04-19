183882c5603a70577b1a104830e1d4678471a4e0
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-04-19 12:00:05 +0200

    Remove temporary migration

--- migrations/versions/69-mypayment.py
+++ migrations/versions/69-mypayment.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "de94c373f94a"
+revision: str = "c4d2aa4e6f1b"
 down_revision: str | None = "c052cfbe6d75"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
