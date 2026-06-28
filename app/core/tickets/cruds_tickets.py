3fb343db247ef9ac0b84a454640a3af80e86209b
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-06-28 17:14:31 +0200

    Remove duplicated count_valid_checkouts_and_tickets_by_event_id definition

--- app/core/tickets/cruds_tickets.py
+++ app/core/tickets/cruds_tickets.py
@@ -800,26 +800,6 @@ async def count_valid_checkouts_by_session_id(
     return result.scalar() or 0
 
 
-async def count_valid_checkouts_and_tickets_by_event_id(
-    event_id: UUID,
-    db: AsyncSession,
-) -> int:
-    """
-    Count unpaid checkouts that are not expired and paid tickets
-    """
-    result = await db.execute(
-        select(func.count()).where(
-            models_tickets.Checkout.event_id == event_id,
-            or_(
-                models_tickets.Checkout.paid,
-                models_tickets.Checkout.expiration >= datetime.now(UTC),
-            ),
-        ),
-    )
-
-    return result.scalar() or 0
-
-
 async def count_valid_checkouts_and_tickets_by_category_id(
     category_id: UUID,
     db: AsyncSession,
